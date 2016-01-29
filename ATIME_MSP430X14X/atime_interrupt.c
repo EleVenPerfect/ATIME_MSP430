7d1f38bbff3dd617f03ca3ec6bf38ee2a02070dc
ATIME <atime@atime.org.cn>
2016-01-29 19:26:11 +0800

    interrupt

---
+++ ATIME_MSP430X14X/atime_interrupt.c
@@ -0,0 +1,67 @@
+/************************************
+库功能：中断服务函数控制库
+库要求：主函数中已定义：
+        #include <msp430x14x.h>
+        #include "atime_msp430core.h"
+应用函数：   
+修改历史：
+		   	 ‘修改人’   ‘修改内容’  ‘修改时间’
+				空			空			空
+最后修改时间：2016-01-25
+作者： ATIME	版权所有
+实例程序：
+常见错误解释：
+1.Error[e46]: Undefined external "uart1_sendchar" referred in main  串口1设置没有打开
+解决：#define UART1_ON 
+2.Warning[Pe223]: function "uart0_sendchar" declared implicitly 
+解决：函数库载入顺序错误，需要将被调用的函数库放在这个控制库前面载入。
+***************************************/
+
+/************************************
+函数功能：串口0接收中断服务函数
+***************************************/
+#pragma vector=UART0RX_VECTOR
+__interrupt void UART0_RX_ISR(void)
+{
+
+}
+
+
+/************************************
+函数功能：串口0发送中断服务函数
+***************************************/
+#pragma vector=UART0TX_VECTOR
+__interrupt void UART0_TX_ISR(void)
+{
+
+}
+
+
+/************************************
+函数功能：串口1接收中断服务函数
+***************************************/
+#pragma vector=UART1RX_VECTOR
+__interrupt void UART1_RX_ISR(void)
+{
+
+}
+
+
+/************************************
+函数功能：串口1发送中断服务函数
+***************************************/
+#pragma vector=UART1TX_VECTOR
+__interrupt void UART1_TX_ISR(void)
+{
+
+}
+
+
+/************************************
+函数功能：ADC12中断服务函数
+***************************************/
+#pragma vector=ADC_VECTOR
+__interrupt void ADC_12_ISR(void)
+{
+
+}
