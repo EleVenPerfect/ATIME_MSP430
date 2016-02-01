22e41a8b2d28997a111834edc3652ae40cb0ad3e
ATIME <atime@atime.org.cn>
2016-02-01 12:26:28 +0800

    spi software

--- ATIME_MSP430X14X/atime_spi_s.h
+++ ATIME_MSP430X14X/atime_spi_s.h
@@ -39,7 +39,7 @@ struct spi_s
 模式0：CPHA=0;CPOL=0;
 模式1：CPHA=0;CPOL=1;
 模式2：CPH1=1;CPOL=0;
-模式3：CPHA=1;CPOL=0;
+模式3：CPHA=1;CPOL=1;
 CPHA:时钟相位设置；
      0表示每周期第一个时钟沿采样，
      1表示每周期第二个时钟沿采样。
@@ -76,6 +76,9 @@ void spi_s_init(struct spi_s a)
     *add &=~(0x01<<a.sdi_b);//设置PxDIR
     add =ioreg_trans(a.sdi_p,3);
     *add &=~(0x01<<a.sdi_b);//设置PxSEL
+    
+    add =ioreg_trans(a.cs_p,1); //CS  线PxOUT 
+    *add |=(0x01<<a.cs_b);//CS 线置高
 }
 
 
@@ -100,11 +103,57 @@ unsigned char transbit( unsigned char x)
 
 
 /************************************
-函数功能：SPI写数据0
-传递参数：空
+函数功能：SPI时钟跳变
+传递参数：a:SPI信息
 返回值：空
 ***************************************/
-unsigned char spi_s_write0( struct spi_s a, unsigned char data)
+void spi_s_clk( struct spi_s a)
+{
+    unsigned char *add,var;
+    
+    add =ioreg_trans(a.clk_p,1); //CLK  线PxOUT0
+    var =*add;
+    if(var&(0x01<<a.clk_b))
+	*add &=~(0x01<<a.clk_b);//CLK线下降沿
+    else
+	*add |=(0x01<<a.clk_b);//CLK线上升沿
+}
+/************************************
+函数功能：SPI读线上数据
+传递参数：a:SPI信息
+返回值：空
+***************************************/
+unsigned char spi_s_miso( struct spi_s a)
+{
+    unsigned char i,*add,temp;
+    i =0;
+    add =ioreg_trans(a.sdi_p,0); //SDI  线PxIN0 
+    temp =*add;//读取 SDI 线数据
+    i |=((temp>>a.sdi_b)&0x01);
+
+    return (i);
+}
+/************************************
+函数功能：SPI写线上数据
+传递参数：a:SPI信息
+返回值：空
+***************************************/
+void spi_s_mosi( struct spi_s a, unsigned char data)
+{
+    unsigned char *add;
+	
+    add =ioreg_trans(a.sdo_p,1);//SDO 线PxOUT
+    if(data&0x80)
+	*add |=(0x01<<a.sdo_b);//SDO线上数据
+    else
+	*add &=~(0x01<<a.sdo_b);//SDO线上数据
+}
+/************************************
+函数功能：SPI写数据
+传递参数：a:SPI信息；data：数据；
+返回值：sdi读取的结果
+***************************************/
+unsigned char spi_s_write( struct spi_s a, unsigned char data)
 {
     unsigned char *add,j,i,temp;
     
@@ -113,87 +162,81 @@ unsigned char spi_s_write0( struct spi_s a, unsigned char data)
     *add &=~(0x01<<a.cs_b);
     
     add =ioreg_trans(a.clk_p,1); //CLK  线PxOUT1 
-    *add |=(0x01<<a.clk_b);
+    if((a.mode&0x01)==0x00)//CPOL=0，则平时为低电平
+	*add &=~(0x01<<a.clk_b);
+    else
+	*add |=(0x01<<a.clk_b);
     
     i =0;
+    temp =data;
     for( j=0; j<8; j++)
     {
-        add =ioreg_trans(a.sdo_p,1);//SDO 线PxOUT
-        if(data&0x80)
-            *add |=(0x01<<a.sdo_b);//SDO线上数据
-        
-        add =ioreg_trans(a.clk_p,1); //CLK  线PxOUT0 
-        *add &=~(0x01<<a.clk_b);//CLK线下降沿
-        
-        add =ioreg_trans(a.sdi_p,0); //SDI  线PxIN0 
-        temp =*add;//读取 SDI 线数据
-        i <<=1;
-        i |=((temp>>a.sdi_b)&0x01);
-        
-        
-        add =ioreg_trans(a.clk_p,1); //CLK  线PxOUT0 
-        *add |=(0x01<<a.clk_b);//CLK线上升沿
-        
-        data <<=1;
+        if((a.mode&0x02)==0x02)
+        {
+            spi_s_clk(a);
+            spi_s_mosi(a,temp);
+            temp<<=1;
+            spi_s_clk(a);
+            i<<=1;
+            i |=spi_s_miso(a);
+        }
+        else
+        {
+            spi_s_mosi(a,temp);
+            temp<<=1;
+            spi_s_clk(a);
+            i<<=1;
+            i |=spi_s_miso(a);
+            spi_s_clk(a);
+        }
     }
-    
     add =ioreg_trans(a.cs_p,1); //CS  线PxOUT 
     *add |=(0x01<<a.cs_b);
     return (i);
 }
-/************************************
-函数功能：SPI写数据1
-传递参数：空
-返回值：空
-***************************************/
-unsigned char spi_s_write1( struct spi_s a, unsigned char data)
-{
-    unsigned char i;
-    
 
-    return (i);
-}
-/************************************
-函数功能：SPI写数据2
-传递参数：空
-返回值：空
-***************************************/
-unsigned char spi_s_write2( struct spi_s a, unsigned char data)
-{
-    unsigned char i;
-    
 
-    return (i);
-}
 /************************************
-函数功能：SPI写数据3
-传递参数：空
-返回值：空
+函数功能：SPI读数据
+传递参数：a:SPI信息
+返回值：读取的结果
 ***************************************/
-unsigned char spi_s_write3( struct spi_s a, unsigned char data)
+unsigned char spi_s_read( struct spi_s a)
 {
-    unsigned char i;
+    unsigned char *add,j,i;
     
-
-    return (i);
-}
-/************************************
-函数功能：SPI写数据
-传递参数：空
-返回值：空
-***************************************/
-unsigned char spi_s_write( struct spi_s a, unsigned char data)
-{
-    unsigned char i;
+    add =ioreg_trans(a.cs_p,1); //CS  线PxOUT 
+    *add |=(0x01<<a.cs_b);
+    *add &=~(0x01<<a.cs_b);
     
-    switch(a.mode)
+    add =ioreg_trans(a.clk_p,1); //CLK  线PxOUT1 
+    if((a.mode&0x01)==0x00)//CPOL=0，则平时为低电平
+	*add &=~(0x01<<a.clk_b);
+    else
+	*add |=(0x01<<a.clk_b);
+    
+    i =0;
+    for( j=0; j<8; j++)
     {
-        case 0 : i=spi_s_write0( a, data);break;
-        case 1 : i=spi_s_write1( a, data);break;
-        case 2 : i=spi_s_write2( a, data);break;
-        case 3 : i=spi_s_write3( a, data);break;
-        default:i=0xff;
+        if((a.mode&0x02)==0x02)
+        {
+            spi_s_clk(a);
+            delay_us(100);
+            spi_s_clk(a);
+            i<<=1;
+            i |=spi_s_miso(a);
+        }
+        else
+        {
+            spi_s_clk(a);
+            i<<=1;
+            i |=spi_s_miso(a);
+            spi_s_clk(a);
+            delay_us(100);
+        }
     }
+    add =ioreg_trans(a.cs_p,1); //CS  线PxOUT 
+    *add |=(0x01<<a.cs_b);
     return (i);
 }
 
