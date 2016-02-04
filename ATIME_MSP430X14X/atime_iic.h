298b539bb422d9eb64953c2c640080b80d553561
ATIME <atime@atime.org.cn>
2016-02-04 22:53:37 +0800

    iic

--- ATIME_MSP430X14X/atime_iic.h
+++ ATIME_MSP430X14X/atime_iic.h
@@ -21,12 +21,30 @@
 /************************************
 库全局变量组
 ***************************************/
-
+#define SDA_PORT        1       //定义SDA总线IO端口
+#define SDA_BIT         1       //定义SDA总线IO引脚
+#define SCL_PORT        1       //定义SCL总线IO端口
+#define SCL_BIT         2       //定义SCL总线IO引脚
+#define IIC_CYC         10      //定义时序的周期
 
 /************************************
-函数功能：初始化SPI接口1
+函数功能：初始化IIC接口
 传递参数：空
 返回值：空
 ***************************************/
+void iic_init()
+{
+    unsigned char *add;
+    add =ioreg_trans(SCL_PORT,2);
+    *add &=~(0x01<<SCL_BIT);
+    add =ioreg_trans(SDA_PORT,2);
+    *add &=~(0x01<<SDA_BIT);//设置为输入状态
+    add =ioreg_trans(SCL_PORT,3);
+    *add &=~(0x01<<SCL_BIT);
+    add =ioreg_trans(SDA_PORT,3);
+    *add &=~(0x01<<SDA_BIT);//设置为IO功能
+}
+
+
 
 #endif
