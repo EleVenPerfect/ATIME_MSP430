8dbf9c79ac6e74fb012e7153774c6f04d2914fc7
ATIME <atime@atime.org.cn>
2016-02-08 22:50:10 +0800

    RS485

--- ATIME_MSP430X14X/atime_main.c
+++ ATIME_MSP430X14X/atime_main.c
@@ -1,7 +1,7 @@
 #include <msp430x14x.h>
 #include "atime_msp430core.h"		//MSP430核心库
 #include "atime_lcd5110_printf.h"
-#include "atime_flash.h"
+#include "atime_rs485.h"
 
 
 void main(void)
@@ -14,22 +14,6 @@ void main(void)
     lcd5110_init();
     printf("Power By ATIME");
 
-    flash_init();                       //初始化flash
-    
-    flash_erase_segment(FLASH_INFO_A);  //写flash前先把该块擦除
-    flash_writeword( FLASH_INFO_A, 0xaabb);//写一个word
-    a =flash_readword(FLASH_INFO_A);    //读一个word
-    printf("%02X\n",a);
-    flash_erase_segment(FLASH_INFO_A);  //写flash前先把该块擦除
-    a =flash_readword(FLASH_INFO_A);    //验证擦除数据
-    printf("%02X\n",a);
-    
-    flash_writechar( FLASH_INFO_A, 0xab);//写一个byte
-    a =flash_readword(FLASH_INFO_A);     //读一个word
-    printf("%02X\n",a);
-    flash_erase_segment(FLASH_INFO_A);   //写flash前先把该块擦除
-    a =flash_readchar(FLASH_INFO_A);     //验证擦除数据
-    printf("%02X\n",a);
     
     while(1);
 }
