427ce5700e7a29e5f89dcdf53329f33e86534e91
ATIME <atime@atime.org.cn>
2016-02-09 23:09:45 +0800

    timer a

--- ATIME_MSP430X14X/ATIME/atime_timer_a.c
+++ ATIME_MSP430X14X/ATIME/atime_timer_a.c
@@ -4,13 +4,17 @@
         #include <msp430x14x.h>
         #include "atime_msp430core.h"
 应用函数：
+        timera_pwm_init1(unsigned long freq)
+        timera_pwm_init2( unsigned char channel, unsigned int duty)
 修改历史：
 	‘修改人’   ‘修改内容’  ‘修改时间’
 	  空	      空	  空
-最后修改时间：2016-02-06
+最后修改时间：2016-02-09
 作者： ATIME	版权所有
 实例程序：
-常见错误解释：
+注：
+1.目前只写了PWM模式的函数，其他功能尚未添加，如有需要欢迎联系atime@atime.org.cn。
+2.timera_pwm_init1要先设置好后再调用timera_pwm_init2，才可以保证占空比正确设置。
 ***************************************/
 
 #ifndef _ATIME_MSP430_TIMER_A_C_ 
@@ -22,6 +26,81 @@
 ***************************************/
 
 
+/************************************
+函数功能：TIMER_A的PWM频率设置与初始化
+传递参数：定时器频率设置
+返回值：空
+***************************************/
+void timera_pwm_init1(unsigned long freq)
+{
+    unsigned long mod =0;
+    unsigned long modtemp =0;
+    unsigned char ps =0;                //分频系数
+	
+    TACTL |=TACLR;                      //清除定时器A
+
+    if((modtemp=(unsigned long)(XT2IN/MSP430_DIVS)/(freq*8))<0xFFFF)
+    {
+        ps =3;
+        mod =modtemp;
+	if((modtemp=(unsigned long)(XT2IN/MSP430_DIVS)/(freq*4))<0xFFFF)
+	{
+            ps =2;
+            mod =modtemp;
+            if((modtemp=(unsigned long)(XT2IN/MSP430_DIVS)/(freq*2))<0xFFFF)
+            {
+                ps =1;
+		mod =modtemp;
+		if((modtemp=(unsigned long)(XT2IN/MSP430_DIVS)/freq)<0xFFFF)
+		{
+                    ps =0;
+		    mod =modtemp;
+		}
+            }
+	 }
+    }
+    TACTL =0;                           //关闭定时器
+    TACTL =TASSEL1 + (ps<<6) + MC_1;
+    TACCR0 =mod;
+}
+
+
+/************************************
+函数功能：PWM输出通道及占空比配置
+传递参数：
+    channel - PWM输出通道
+      |__0--TA1
+      |__1--TA2
+    duty - PWM输出占空比
+      |__0~10000--占空比0.00%~100.00%
+返回值：空
+注意：timera_pwm_init1要先设置好后再调用timera_pwm_init2，才可以保证占空比正确设置。
+***************************************/
+void timera_pwm_init2( unsigned char channel, unsigned int duty)
+{
+    switch(channel)
+    {
+          case 0:
+          {
+              TACCTL1 =OUTMOD_6;
+              TACCR1 =(unsigned int)((duty*TACCR0)/10000);
+          }
+          break;
+          case 1:
+          {
+              TACCTL2 =OUTMOD_6;
+              TACCR2 =(unsigned int)((duty*TACCR0)/10000);
+          }
+          break;
+    }
+}
+
+
+/************************************
+函数功能：
+传递参数：空
+返回值：空
+***************************************/
 /************************************
 函数功能：
 传递参数：空
--- ATIME_MSP430X14X/atime_main.c
+++ ATIME_MSP430X14X/atime_main.c
@@ -1,20 +1,22 @@
 #include <msp430x14x.h>
 #include "atime_msp430core.h"		//MSP430核心库
 #include "atime_lcd5110_printf.h"
-#include "atime_rs485.h"
+#include "atime_timer_a.h"
 #include "atime_interrupt.c"
 
 
 void main(void)
 {
-    //unsigned char data=0;
-    
     watchdog_close();			//关闭看门狗
     wait_ms(50);
     basic_clock_init();			//系统时钟初始化
     lcd5110_init();
     printf("Power By ATIME");
-    rs485_init();
-    interrupt_switch(on);  
+
+    //interrupt_switch(on);
+
+    timera_pwm_init1(1000);
+    timera_pwm_init2(1,6666);
+    
     while(1);
 }
