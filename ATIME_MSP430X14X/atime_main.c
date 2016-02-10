40c8357ca06986e5836d0366813ce7546fed5b5f
ATIME <atime@atime.org.cn>
2016-02-10 09:43:59 +0800

    timer b

--- ATIME_MSP430X14X/atime_main.c
+++ ATIME_MSP430X14X/atime_main.c
@@ -1,26 +1,16 @@
 #include <msp430x14x.h>
 #include "atime_msp430core.h"		//MSP430核心库
-//#include "atime_lcd5110_printf.h"
-#include "atime_timer_b.h"
+#include "atime_lcd5110_printf.h"
+
 
 
 void main(void)
 {
-    unsigned int i=1;
     watchdog_close();			//关闭看门狗
     wait_ms(50);
     basic_clock_init();			//系统时钟初始化
-    //lcd5110_init();
-    //printf("Power By ATIME");
+    lcd5110_init();
+    printf("Power By ATIME");
 
-    timerb_pwm_init1(1000);
-    while(1)
-    {
-        i+=1000;
-        if(i>9999)
-            i=0;
-        timerb_pwm_init2(0,i);
-        timerb_pwm_init2(1,10000-i);
-        wait_ms(500);
-    }
+    while(1);
 }
