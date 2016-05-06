23fd22bbb70130991b751f9bd519c3136aab20b4
ATIME <atime@atime.org.cn>
2016-05-06 22:37:38 +0800

    err

--- demo/demo1/ATIME/atime_am2320.c
+++ demo/demo1/ATIME/atime_am2320.c
@@ -22,8 +22,147 @@
 /************************************
 库全局变量组
 ***************************************/
-int am2320_w = 0;               //温度数据
-int am2320_s = 0;               //湿度数据
+int am2320_w = 0;                       //温度数据
+int am2320_s = 0;                       //湿度数据
+
+#define AM2320_1_SDA_PORT        3       //定义SDA总线IO端口
+#define AM2320_1_SDA_BIT         1       //定义SDA总线IO引脚
+#define AM2320_1_SCL_PORT        3       //定义SCL总线IO端口
+#define AM2320_1_SCL_BIT         0       //定义SCL总线IO引脚
+
+/************************************
+函数功能：IIC接口开始
+传递参数：空
+返回值：空
+***************************************/
+void am2320_1_iic_start_s()
+{
+    PxyDIRz(AM2320_1_SCL_PORT,AM2320_1_SCL_BIT,1);
+    PxyDIRz(AM2320_1_SDA_PORT,AM2320_1_SDA_BIT,1);
+    
+    PxyOUTz(AM2320_1_SCL_PORT,AM2320_1_SCL_BIT,1);
+    PxyOUTz(AM2320_1_SDA_PORT,AM2320_1_SDA_BIT,1);
+    delay_us(50);
+    
+    PxyOUTz(AM2320_1_SDA_PORT,AM2320_1_SDA_BIT,0);
+    delay_us(50);
+    PxyOUTz(AM2320_1_SCL_PORT,AM2320_1_SCL_BIT,0);
+    delay_us(10);
+}
+
+/************************************
+函数功能：IIC接口结束
+传递参数：空
+返回值：空
+***************************************/
+void am2320_1_iic_stop_s()
+{
+    PxyDIRz(AM2320_1_SCL_PORT,AM2320_1_SCL_BIT,1);
+    PxyDIRz(AM2320_1_SDA_PORT,AM2320_1_SDA_BIT,1);
+    delay_us(15);
+    PxyOUTz(AM2320_1_SCL_PORT,AM2320_1_SCL_BIT,1);
+    PxyOUTz(AM2320_1_SDA_PORT,AM2320_1_SDA_BIT,0);
+    delay_us(15);
+    PxyOUTz(AM2320_1_SDA_PORT,AM2320_1_SDA_BIT,1);
+    delay_us(30);
+}
+
+
+/************************************
+函数功能：IIC接口接收ACKNOWLEDGE
+传递参数：空
+返回值：ACK
+***************************************/
+unsigned char am2320_1_iic_getack_s()
+{
+    unsigned char ack=1;
+    PxyDIRz(AM2320_1_SCL_PORT,AM2320_1_SCL_BIT,1);
+    PxyDIRz(AM2320_1_SDA_PORT,AM2320_1_SDA_BIT,0);
+    
+    PxyOUTz(AM2320_1_SCL_PORT,AM2320_1_SCL_BIT,0);
+    delay_us(10);
+    PxyOUTz(AM2320_1_SCL_PORT,AM2320_1_SCL_BIT,1);
+    delay_us(10);
+    ack =PxyINz(AM2320_1_SDA_PORT,AM2320_1_SDA_BIT);
+    PxyOUTz(AM2320_1_SCL_PORT,AM2320_1_SCL_BIT,0);
+    return ack;
+}
+/************************************
+函数功能：IIC接口发送ACKNOWLEDGE
+传递参数：空
+返回值：空
+***************************************/
+void am2320_1_iic_setack_s()
+{
+    PxyDIRz(AM2320_1_SCL_PORT,AM2320_1_SCL_BIT,1);
+    PxyDIRz(AM2320_1_SDA_PORT,AM2320_1_SDA_BIT,1);
+    
+    PxyOUTz(AM2320_1_SCL_PORT,AM2320_1_SCL_BIT,0);
+    PxyOUTz(AM2320_1_SDA_PORT,AM2320_1_SDA_BIT,0);
+    delay_us(10);
+    PxyOUTz(AM2320_1_SCL_PORT,AM2320_1_SCL_BIT,1);
+    delay_us(30);
+    PxyOUTz(AM2320_1_SCL_PORT,AM2320_1_SCL_BIT,0);
+    PxyOUTz(AM2320_1_SDA_PORT,AM2320_1_SDA_BIT,0);
+    delay_us(10);
+}
+
+/************************************
+函数功能：IIC接口读byte
+传递参数：空
+返回值：读取数据
+注：接收数据低位先接收
+***************************************/
+unsigned char am2320_1_iic_readbyte_s()
+{
+    unsigned char i,data=0,temp;
+    PxyDIRz(AM2320_1_SCL_PORT,AM2320_1_SCL_BIT,1);
+    PxyDIRz(AM2320_1_SDA_PORT,AM2320_1_SDA_BIT,0);
+    for( i=0; i<8; i++)
+    {
+        PxyOUTz(AM2320_1_SCL_PORT,AM2320_1_SCL_BIT,1);
+	delay_us(20);
+        data <<=1;
+        temp =PxyINz(AM2320_1_SDA_PORT,AM2320_1_SDA_BIT);
+        if(temp)
+            data |=0x01;
+        else
+            data &=~(0x01);
+        PxyOUTz(AM2320_1_SCL_PORT,AM2320_1_SCL_BIT,0);
+        delay_us(10);
+    }
+    return data;
+}
+
+
+/************************************
+函数功能：IIC接口写byte
+传递参数：发送数据
+返回值：空
+注：发送数据高位先发送
+***************************************/
+void am2320_1_iic_writebyte_s(unsigned char data)
+{
+    unsigned char i;
+    PxyDIRz(AM2320_1_SCL_PORT,AM2320_1_SCL_BIT,1);
+    PxyDIRz(AM2320_1_SDA_PORT,AM2320_1_SDA_BIT,1);
+    
+    for( i=0; i<8; i++)
+    {
+        if(data&0x80)
+            PxyOUTz(AM2320_1_SDA_PORT,AM2320_1_SDA_BIT,1);
+        else
+            PxyOUTz(AM2320_1_SDA_PORT,AM2320_1_SDA_BIT,0);
+		delay_us(20);
+        PxyOUTz(AM2320_1_SCL_PORT,AM2320_1_SCL_BIT,1);
+        delay_us(40);
+        PxyOUTz(AM2320_1_SCL_PORT,AM2320_1_SCL_BIT,0);
+        delay_us(15);
+        data <<=1;
+        PxyOUTz(AM2320_1_SDA_PORT,AM2320_1_SDA_BIT,0);
+    }
+}
+
 
 
 /************************************
@@ -31,13 +170,13 @@ int am2320_s = 0;               //湿度数据
 传递参数：数据存储数组
 返回值：空
 ***************************************/
-void as2320_waken_iic(void)
+void as2320_1_waken_iic(void)
 {
-    iic_start_s();
-    iic_writebyte_s(0xB8);
-    iic_getack_s();
+    am2320_1_iic_start_s();
+    am2320_1_iic_writebyte_s(0xB8);
+    am2320_1_iic_getack_s();
     wait_ms(2);
-    iic_stop_s();
+    am2320_1_iic_start_s();
 }
 
 
@@ -46,29 +185,29 @@ void as2320_waken_iic(void)
 传递参数：数据存储数组
 返回值：空
 ***************************************/
-void as2320_read_iic(void)
+void as2320_1_read_iic(void)
 {
     unsigned char am2320_data[8]={0};
-    iic_start_s();
-    iic_writebyte_s(0xB8|0x01);
-    iic_getack_s();
+    am2320_1_iic_start_s();
+    am2320_1_iic_writebyte_s(0xB8|0x01);
+    am2320_1_iic_getack_s();
     wait_ms(1);
-    am2320_data[0] = iic_readbyte_s();
-    iic_setack_s();
-    am2320_data[1] = iic_readbyte_s();
-    iic_setack_s();
-    am2320_data[2] = iic_readbyte_s();
-    iic_setack_s();
-    am2320_data[3] = iic_readbyte_s();
-    iic_setack_s();
-    am2320_data[4] = iic_readbyte_s();
-    iic_setack_s();
-    am2320_data[5] = iic_readbyte_s();
-    iic_setack_s();
-    am2320_data[6] = iic_readbyte_s();
-    iic_setack_s();
-    am2320_data[7] = iic_readbyte_s();
-    iic_stop_s();
+    am2320_data[0] = am2320_1_iic_readbyte_s();
+    am2320_1_iic_setack_s();
+    am2320_data[1] = am2320_1_iic_readbyte_s();
+    am2320_1_iic_setack_s();
+    am2320_data[2] = am2320_1_iic_readbyte_s();
+    am2320_1_iic_setack_s();
+    am2320_data[3] = am2320_1_iic_readbyte_s();
+    am2320_1_iic_setack_s();
+    am2320_data[4] = am2320_1_iic_readbyte_s();
+    am2320_1_iic_setack_s();
+    am2320_data[5] = am2320_1_iic_readbyte_s();
+    am2320_1_iic_setack_s();
+    am2320_data[6] = am2320_1_iic_readbyte_s();
+    am2320_1_iic_setack_s();
+    am2320_data[7] = am2320_1_iic_readbyte_s();
+    am2320_1_iic_start_s();
     if( am2320_data[0]==0x03&&am2320_data[1]==0x04 )
     {
 
@@ -88,18 +227,18 @@ void as2320_read_iic(void)
 传递参数：数据存储数组
 返回值：空
 ***************************************/
-void am2320_write_iic(void)
+void am2320_1_write_iic(void)
 {
-    iic_start_s();
-    iic_writebyte_s(0xB8);
-    iic_getack_s();
-    iic_writebyte_s(0x03);
-    iic_getack_s();
-    iic_writebyte_s(0x00);
-    iic_getack_s();
-    iic_writebyte_s(0x04);
-    iic_getack_s();
-    iic_stop_s();
+    am2320_1_iic_start_s();
+    am2320_1_iic_writebyte_s(0xB8);
+    am2320_1_iic_getack_s();
+    am2320_1_iic_writebyte_s(0x03);
+    am2320_1_iic_getack_s();
+    am2320_1_iic_writebyte_s(0x00);
+    am2320_1_iic_getack_s();
+    am2320_1_iic_writebyte_s(0x04);
+    am2320_1_iic_getack_s();
+    am2320_1_iic_start_s();
 }
 
 
@@ -108,12 +247,12 @@ void am2320_write_iic(void)
 传递参数：数据存储数组
 返回值：空
 ***************************************/
-void as2320_read()
+void as2320_1_read()
 {
-    as2320_waken_iic();
-    am2320_write_iic();
+    as2320_1_waken_iic();
+    am2320_1_write_iic();
     wait_ms(3);
-    as2320_read_iic();
+    as2320_1_read_iic();
 }
 
 
--- demo/demo1/atime_main.c
+++ demo/demo1/atime_main.c
@@ -1,7 +1,7 @@
 #include <msp430x14x.h>
 #include "atime_msp430core.h"		//MSP430核心库
 #include "atime_lcd5110_printf.h"
-#include "atime_iic_software.h"
+//#include "atime_iic_software.h"
 #include "atime_am2320.c"
 
 void main(void)
@@ -15,7 +15,7 @@ void main(void)
     while(1)
     {
         wait_ms(2500);
-        as2320_read();
+        as2320_1_read();
         printf("%d\n",am2320_w);//以16进制显示
         printf("%d\n",am2320_s);//以16进制显示
 
