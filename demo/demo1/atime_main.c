ba018b1fc8ff2da978d5ef4ef9c593acc29978b3
ATIME <atime@atime.org.cn>
2016-05-07 18:20:17 +0800

    am2320.c

--- demo/demo1/atime_main.c
+++ demo/demo1/atime_main.c
@@ -1,23 +1,21 @@
 #include <msp430x14x.h>
 #include "atime_msp430core.h"		//MSP430核心库
 #include "atime_lcd5110_printf.h"
-//#include "atime_iic_software.h"
-#include "atime_am2320.c"
+#include "atime_am2320.h"
 
 void main(void)
 {
     watchdog_close();			//关闭看门狗
-    wait_ms(50);
+    wait_ms(50);                        //等待不稳定状态
     basic_clock_init();			//系统时钟初始化
     lcd5110_init();                     //液晶屏幕初始化
-
 
     while(1)
     {
         wait_ms(2500);
         as2320_read();
-        printf("%d\n",am2320_w);//以16进制显示
-        printf("%d\n",am2320_s);//以16进制显示
+        printf("%d\n",am2320_1_w);      //显示温度数据*10
+        printf("%d\n",am2320_1_s);      //显示湿度数据*10
 
         printf_setadd(0,0);
     } 
