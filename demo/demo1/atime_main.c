6060d0ba87805b0acd57acbc4365113e4943a728
ATIME <atime@atime.org.cn>
2016-06-10 20:46:30 +0800

    demo

--- demo/demo1/atime_main.c
+++ demo/demo1/atime_main.c
@@ -1,6 +1,6 @@
 #include <msp430x14x.h>
 #include "atime_msp430core.h"		//MSP430核心库
-#include "atime_lcd5110_printf.h"
+//#include "atime_lcd5110_printf.h"
 #include "atime_lcd_hmi.c"
 #include "atime_iic_software.h"
 #include "atime_ds1307.c"
@@ -11,14 +11,18 @@
 void main(void)
 {
     unsigned char i=0;
+    
     watchdog_close();			//关闭看门狗
     wait_ms(50);                        //等待不稳定状态
     basic_clock_init();			//系统时钟初始化
-    lcd5110_init();                     //液晶屏幕初始化
+    //lcd5110_init();                     //液晶屏幕初始化
     uart_init();                        //串口初始化
     interrupt_switch(on);               //开总中断
     lcd_order("rest");
     lcd_order("rest");                  //第一次发指令需要写两遍，保证指令正确发出
+    
+    ds1307_write(ds1307_data);
+
     while(1)
     {
         ds1307_read(ds1307_data);
@@ -36,14 +40,14 @@ void main(void)
             am2320_3_read();
         }
         
-        printf("%d \n",am2320_1_w);      //显示温度数据*10
-        printf("%d \n",am2320_1_s);      //显示湿度数据*10
-        printf("%d \n",am2320_2_w);      //显示温度数据*10
-        printf("%d \n",am2320_2_s);      //显示湿度数据*10
-        printf("%d \n",am2320_3_w);      //显示温度数据*10
-        printf("%d \n",am2320_3_s);      //显示湿度数据*10
+        //printf("%d \n",am2320_1_w);      //显示温度数据*10
+        //printf("%d \n",am2320_1_s);      //显示湿度数据*10
+        //printf("%d \n",am2320_2_w);      //显示温度数据*10
+        //printf("%d \n",am2320_2_s);      //显示湿度数据*10
+        //printf("%d \n",am2320_3_w);      //显示温度数据*10
+        //printf("%d \n",am2320_3_s);      //显示湿度数据*10
         
-        printf_setadd(0,0);        
+        //printf_setadd(0,0);        
 
         lcd_num_update(0,am2320_1_w);
         lcd_num_update(1,am2320_1_s);
